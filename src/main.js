92742df8cf36f499eb927f6fae81f32eb9d52fbc
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2024-12-12 22:52:46 +0900

    migrate cli to vite

--- src/main.js
+++ src/main.js
@@ -4,19 +4,12 @@ import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
 import router from './router/index.js';
 import { MotionPlugin } from '@vueuse/motion';
-import ScrollReveal from 'scrollreveal';
 import store from '@/store';
 import axios from 'axios';
 import './index.css';
+import AOS from 'aos';
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
 loadFonts();
-ScrollReveal({ reset: false, distance: '80px', duration: 2000, delay: 100 });
-createApp(App)
-  .use(router)
-  .use(ScrollReveal)
-  .use(store)
-  .use(MotionPlugin)
-  .use(vuetify)
-  .mount('#app');
+createApp(App).use(router).use(store).use(MotionPlugin).use(vuetify).use(AOS).mount('#app');
