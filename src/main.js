60c5b72b808f070c7265e013748579aea65eb352
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2024-12-12 23:22:31 +0900

    install dependece

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
-import store from 'my-vue-app/src/store';
+import store from './store';
 import axios from 'axios';
-import router from '../my-vue-app/src/router/index.js';
+import router from './router/index.js';
 import './index.css';
 
-import { MotionPlugin } from '@vueuse/motion';
-
 axios.defaults.baseURL = process.env.VUE_APP_API_URL;
-createApp(App).use(MotionPlugin).use(router).use(store).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
