99f1fe528930599920140700c9d20ae22daeea0b
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2024-12-16 14:51:10 +0900

    update: versi 1.1.0

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
-// import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-//   define: {
-//     'process.env': JSON.stringify(process.env),
-//   },
-// });
-
-import { defineConfig, loadEnv } from 'vite';
-
 export default defineConfig(({ mode }) => {
   // Load env variables
-  const env = loadEnv(mode, process.cwd(), ''); // Load only VITE_ prefixed variables
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [vue()],
